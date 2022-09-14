2b79aefe511f5e67fd1a53faf5fd54965f44e766
Yahya Hafez <yhafez3@gmail.com>
2022-09-14 01:24:56 -0500

    finish suggested accounts component

--- components/LikeButton.tsx
+++ components/LikeButton.tsx
@@ -25,7 +25,7 @@ const LikeButton = ({ likes, handleLike, handleDislike }: IProps) => {
     }, [filterLikes, likes]);
 
     return (
-        <div className="gap-6">
+        <div className="flex gap-6">
             <div className="mt-4 flex flex-col justify-center items-center cursor-pointer">
                 {alreadyLiked ? (
                     <div
